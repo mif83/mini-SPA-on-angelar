7dffdadfdec4a7ddbf095ec3c48cbb5bad5cd376
Dima Esipenko <mif83@mail.ru>
2017-03-29 01:55:44 +0300

    add template

--- js/my.js
+++ js/my.js
@@ -2,18 +2,42 @@
  * Created by dima on 28.03.2017.
  */
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',{
+            templateUrl:'search.html'
+        })
+        .when('/search/:text/page/:pagenamber', {
+            templateUrl: 'search-result.html',
+            controller: 'firstCtrl'
+        })
+        .otherwise({
+            template: '<h1>404 no such page</h1>'
+        })
+});
 
-app.factory('myFactory', function () {
-    return {
+app.controller('firstCtrl', function($http, $scope, $location) {
+ $scope.startSearch = function (text,num) {
 
-        }
-    }
-})
+     var num = num || "1";
+     $http.get('https://api.github.com/search/repositories?q='+text+'&per_page=10&page='+num)
+         .then(function (result) {
+             console.log('success', result);
+             $scope.result = result.data.items;
+             $location.path("search/"+$scope.search+"/page/"+ num);
+            // $scope.search = null;
+         })
+         .catch(function (err) {
+             console.log(err);
+         })
 
-app.controller('firstCtrl', function($scope, myFactory) {
-
+ };
+ $scope.pressEnter = function (event) {
+     if (event.which === 13)
+         $scope.startSearch($scope.search);
+ }
 
 
 
