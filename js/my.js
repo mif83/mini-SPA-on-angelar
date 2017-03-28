a74f6559fa57de9f8579c4503328200afd47d76b
Dima Esipenko <mif83@mail.ru>
2017-03-28 14:54:44 +0300

    initial commit

---
+++ js/my.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dima on 28.03.2017.
+ */
+
+var app = angular.module('app', []);
+
+
+app.factory('myFactory', function () {
+    return {
+
+        }
+    }
+})
+
+app.controller('firstCtrl', function($scope, myFactory) {
+
+
+
+
+});
